94cc99e3a15175e92e64dc2e235995cea8b4f9e8
shaharheyman <shahar@theheymans.com>
2025-11-11 18:32:07 +0100

    Add comment

--- src/preprocessors.py
+++ src/preprocessors.py
@@ -11,6 +11,7 @@ class ImagePreprocessor(torch.nn.Module):
         self.config: Config = config
         self.resize: T.Resize = T.Resize(self.config.image_size)
         self.to_tensor: T.ToTensor = T.ToTensor()
+        # Original ImageNet normalization values
         self.normalize: T.Normalize = T.Normalize(mean=[0.485, 0.456, 0.406],
                                                   std=[0.229, 0.224, 0.225])
         self.transform: T.Compose = T.Compose([
